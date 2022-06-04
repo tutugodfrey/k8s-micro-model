9fb48e20f9a90bcc3241616410e0f6914779417a
tutugodfrey <godfrey_tutu@yahoo.com>
2022-06-04 01:46:15 +0100

    Added kubernetes.out, .circleci/config.yml and modify run_kubernetes.sh

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 # This tags and uploads an image to Docker Hub
+if [ -z $PORT ]; then
+  PORT=8000
+fi
+
 if [ -z $IMAGE_NAME ]; then
     IMAGE_NAME=predictor
 fi
@@ -13,18 +17,21 @@ if [ -z $IMAGE_REPO ]; then
     IMAGE_REPO=tutug
 fi
 
+echo $IMAGE_NAME
+echo $IMAGE_TAG
+
 # Step 1:
 # This is your Docker ID/path
-dockerpath="$IMAGE_REPO/$IMAGE_NAME:$IMAGE_TAG"
+dockerpath=$IMAGE_REPO/$IMAGE_NAME:$IMAGE_TAG
 containername="boston-house-predictor"
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run $contanername --image= $dockerpath
-
+kubectl create deployment $containername --image=$dockerpath
+# kubectl run boston-house-predictor --image=tutug/predictor:latest
 # Step 3:
 # List kubernetes pods
 kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward 8000:8000 $containername
+kubectl port-forward $containername $PORT:80
